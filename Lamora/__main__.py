fd3b611326d094328e76501991209983b2619620
Lamora <itsmerick184@gmail.com>
2025-05-16 04:12:16 +0700

    Update __main__.py

--- Lamora/__main__.py
+++ Lamora/__main__.py
@@ -1,1 +1,9 @@
-#
+from telethon import TelegramClient
+from config import api_id, api_hash, bot_token
+from handlers import commands
+
+client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
+commands.register(client)
+
+print("Bot berjalan...")
+client.run_until_disconnected()
