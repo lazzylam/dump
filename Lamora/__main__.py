beadb34557782a8db0755a3c9ef91b1041d585af
Lamora <itsmerick184@gmail.com>
2025-05-16 05:15:05 +0800

    Update __main__.py

--- Lamora/__main__.py
+++ Lamora/__main__.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
-from config import api_id, api_hash, bot_token
-from handlers import commands
+from lamora.config import api_id, api_hash, bot_token
+from handlers import com
 
 client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 commands.register(client)
