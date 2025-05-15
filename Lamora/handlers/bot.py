b39b28fff240cea7eb32ef08255672121891439b
Lamora <itsmerick184@gmail.com>
2025-05-16 05:32:54 +0800

    Update bot.py

--- Lamora/handlers/bot.py
+++ Lamora/handlers/bot.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import sys
 from telethon import events
-from utils.helpers import admin_only
+from lamora.utils.tol import admin_only
 
 def register(client):
     @client.on(events.NewMessage(pattern="/reboot"))
@@ -15,9 +15,9 @@ def register(client):
 
         await msg.edit(f"```{output.strip()}```\n\nGit pull berhasil, mematikan bot...")
 
-        # Simpan chat_id untuk notifikasi setelah reboot
+        # Simpan chat_id untuk notifikasi setelah restart
         with open("restart_flag.txt", "w") as f:
             f.write(str(event.chat_id))
 
         await asyncio.sleep(2)
-        os.execv(sys.executable, [sys.executable, "main.py"])
+        os.execv(sys.executable, [sys.executable, "-m", "lamora"])
